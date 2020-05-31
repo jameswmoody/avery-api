f03d778cbcd6d3e80746542654d68b67aba8a11d
James W Moody <james@wmoody.me>
2020-05-31 11:50:20 -0700

    Add endpoint for getting specific user

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,13 +1,8 @@
 'use strict';
 
 const firebase = require('firebase');
-const {
-    db
-} = require('../util/admin');
-const {
-    isEmpty,
-    isEmail
-} = require('../util/helpers');
+const { db } = require('../util/admin');
+const { isEmpty, isEmail } = require('../util/helpers');
 
 const newUser = (req, res) => {
     const {
@@ -94,7 +89,49 @@ const newUser = (req, res) => {
                 errors: err
             });
         });
-}
+};
+
+const getUser = (req, res) => {
+    const userID = req.params.userID;
+    db.doc(`/users/${req.params.userID}`)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                return {
+                    userID,
+                    name: doc.data().name,
+                    photoURL: doc.data().photoURL,
+                    gender: doc.data().gender,
+                    dateOfBirth: doc.data().dateOfBirth,
+                    dateOfDeath: doc.data().dateOfDeath,
+                    address: doc.data().address,
+                    facebook: doc.data().facebook,
+                    instagram: doc.data().instagram,
+                    email: doc.data().email,
+                    phone: doc.data().phone,
+                    mother: doc.data().mother,
+                    father: doc.data().father,
+                    documents: doc.data().documents,
+                    description: doc.data().description,
+                    isUser: doc.data().isUser,
+                    isDeactivated: doc.data().isDeactivated,
+                    isAdmin: doc.data().isAdmin,
+                    createdAt: doc.data().createdAt,
+                    deactivatedAt: doc.data().deactivatedAt
+                };
+            } else {
+                return res
+                    .status(404)
+                    .json({ error: `User ${userId} not found` });
+            }
+        })
+        .then((userData) => {
+            return res.json(userData);
+        })
+        .catch((err) => {
+            return err;
+        });
+};
 
 const getAllUsers = (req, res) => {
     return db
@@ -117,7 +154,7 @@ const getAllUsers = (req, res) => {
                     instagram: doc.data().instagram,
                     email: doc.data().email,
                     phone: doc.data().phone,
-                    mather: doc.data().mother,
+                    mother: doc.data().mother,
                     father: doc.data().father,
                     documents: doc.data().documents,
                     description: doc.data().description,
@@ -133,9 +170,10 @@ const getAllUsers = (req, res) => {
         .catch((err) => {
             console.error(err);
         });
-}
+};
 
 module.exports = {
     newUser,
+    getUser,
     getAllUsers
 };
--- functions/index.js
+++ functions/index.js
@@ -9,27 +9,21 @@ const login = express();
 
 firebase.initializeApp(firebaseConfig);
 
-const {
-    firebaseAuth
-} = require('./util/firebaseAuth');
-const {
-    newPost,
-    getAllPosts
-} = require('./handlers/posts');
-const {
-    newUser,
-    getAllUsers
-} = require('./handlers/users');
-const {
-    loginUser
-} = require('./handlers/login');
+const { firebaseAuth } = require('./util/firebaseAuth');
+const { newPost, getAllPosts } = require('./handlers/posts');
+const { newUser, getUser, getAllUsers } = require('./handlers/users');
+const { loginUser } = require('./handlers/login');
 
+// Posts
 app.get('/api/v1/posts', getAllPosts);
 app.post('/api/v1/posts', firebaseAuth, newPost);
 
+// Users
 app.get('/api/v1/users', getAllUsers);
+app.get('/api/v1/users/:userID', getUser);
 app.post('/api/v1/users', newUser);
 
+// Login
 login.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
